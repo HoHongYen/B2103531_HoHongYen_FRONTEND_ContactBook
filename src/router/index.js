e9f84459ec1fa0dc66da81bc010b65d64e2b9629
HongYen <yenvt9a1@gmail.com>
2023-03-12 09:45:48 +0700

    Cai dat trang loi 404

--- src/router/index.js
+++ src/router/index.js
@@ -7,6 +7,11 @@ const routes = [
 		name: 'contactbook',
 		component: ContactBook,
 	},
+	{
+		path: '/:pathMatch(.*)*',
+		name: 'notfound',
+		component: () => import('@/views/NotFound.vue'),
+	}
 ];
 
 const router = createRouter({
---
+++ src/views/NotFound.vue
@@ -0,0 +1,8 @@
+<template>
+	<div class="page">
+		<p>
+			Oops, không thể tìm thấy trang. Trở về
+			<router-link to="/">trang chủ.</router-link>
+		</p>
+	</div>
+</template>
