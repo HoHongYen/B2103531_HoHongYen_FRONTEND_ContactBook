a56af6e5f784054d1abbe9c97d11aaf6aaf70a72
HongYen <yenvt9a1@gmail.com>
2023-03-13 10:16:20 +0700

    oke

--- src/views/ContactBook.vue
+++ src/views/ContactBook.vue
@@ -98,7 +98,7 @@ export default {
 		filteredContacts() {
 			if (!this.searchText) return this.contacts;
 			return this.contacts.filter((_contact, index) =>
-				this.contactStrings[index].includes(this.searchText)
+				this.contactStrings[index].toLowerCase().includes(this.searchText)
 			);
 		},
 		activeContact() {
